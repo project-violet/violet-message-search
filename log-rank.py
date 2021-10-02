9d06a7bd946900834d531fcc5938c66af592d8c8
violet-dev <violet.dev.master@gmail.com>
2021-10-02 00:14:39 +0000

    rank: Add log rank

--- log-rank.py
+++ log-rank.py
@@ -10,32 +10,49 @@ from os.path import isfile, join
 import re
 import json
 
-f = open('nohup.out', 'r', encoding="UTF-8")
-lines = f.readlines()
-f.close()
-
 contains = {}
 similar = {}
 
-for line in lines:
-    if not line.startswith('('):
-        continue
+def process(filename):
+    f = open(filename, 'r', encoding="UTF-8")
+    lines = f.readlines()
+    f.close()
 
-    data = line.split(')', 1)[1].strip().rsplit('|', 1)[0].strip()
+    for line in lines:
+        if line.startswith('('):
+            data = line.split(')', 1)[1].strip().rsplit('|', 1)[0].strip()
 
-    sc = data.split(':')[0].strip()
-    msg = data.split(':',1)[1].strip()
+            sc = data.split(':')[0].strip()
+            msg = data.split(':',1)[1].strip()
 
-    if sc == 'contains':
-        if not msg in contains:
-            contains[msg] = 0
-        contains[msg] += 1
-    elif sc == 'similar':
-        if not msg in similar:
-            similar[msg] = 0
-        similar[msg] += 1
-    else:
-        print('error')
+            if sc == 'contains':
+                if not msg in contains:
+                    contains[msg] = 0
+                contains[msg] += 1
+            elif sc == 'similar':
+                if not msg in similar:
+                    similar[msg] = 0
+                similar[msg] += 1
+            else:
+                print('error')
+        elif line.split(':')[0] == 'contains' or line.split(':')[0] == 'similar':
+            sc = line.split(':')[0].strip()
+            msg = line.split(':',1)[1].strip()
+
+            if sc == 'contains':
+                if not msg in contains:
+                    contains[msg] = 0
+                contains[msg] += 1
+            elif sc == 'similar':
+                if not msg in similar:
+                    similar[msg] = 0
+                similar[msg] += 1
+            else:
+                print('error')
+
+
+process('nohup-legacy.out')
+process('nohup.out')
 
 def save(name, what):
     what = dict(sorted(what.items(), key=lambda item: -item[1]))
@@ -46,3 +63,13 @@ def save(name, what):
 
 save('contains.json', contains)
 save('similar.json', similar)
+
+with open('README.md', 'w', encoding='UTF-8') as file:
+    c = dict(sorted(contains.items(), key=lambda item: -item[1]))
+    s = dict(sorted(similar.items(), key=lambda item: -item[1]))
+    
+    file.write('# Log Rank\n## Contains\n```\n')
+    file.write(json.dumps(c, ensure_ascii=False, indent=4))
+    file.write('\n```\n## Similar\n```\n')
+    file.write(json.dumps(s, ensure_ascii=False, indent=4))
+    file.write('\n```')
