ca1599b54d4bcce5b6c097e7bcbad730033735cb
violet-dev <violet.dev.master@gmail.com>
2021-10-19 22:40:12 +0000

    cache-merger: implements splitter

--- hcache-merger/Main.cpp
+++ hcache-merger/Main.cpp
@@ -17,11 +17,22 @@
 
 const std::string target_dir = "/home/ubuntu/htext-miner/cache";
 
-int main() {
-  nlohmann::json result;
-  int count {};
-   for (auto& p : std::filesystem::directory_iterator(target_dir))
-      ++count;
+int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    std::cout << argv[0] << " <count of split file>";
+    return 0;
+  }
+
+  int count_of_split_file = atoi(argv[1]);
+
+  std::vector<nlohmann::json *> result;
+
+  for (int i = 0; i < count_of_split_file; i++)
+    result.push_back(new nlohmann::json());
+
+  int count{};
+  for (auto &p : std::filesystem::directory_iterator(target_dir))
+    ++count;
   int count_process = 0;
   for (const auto &entry : std::filesystem::directory_iterator(target_dir)) {
     std::cout << ++count_process << '/' << count << entry.path() << std::endl;
@@ -31,8 +42,11 @@ int main() {
     i >> j;
 
     for (const auto &item : j.items())
-        result.push_back(item.value());
+      result[(count_process - 1) % count_of_split_file]->push_back(item.value());
   }
-  std::ofstream o("merged.json");
-  o << result << std::endl;
+
+  for (int i = 0; i < count_of_split_file; i++) {
+    std::ofstream o(std::string("merged-") + std::to_string(i) + std::string(".json"));
+    o << *result[i] << std::endl;
+  }
 }
---
+++ hcache-merger/build/CMakeFiles/hcache-merger.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hcache-merger.dir/Main.cpp.o"
+  "hcache-merger"
+  "hcache-merger.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hcache-merger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
